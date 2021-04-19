b1ddcc1cb4bf5832bf21130befa18184f15b861d
dengdeng <dengdeng@pinduoduo.com>
2021-04-19 14:51:16 +0800

    first upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+dependencies = [
+    'seaborn',
+    'statsmodels',
+    'scipy',
+    'patsy',
+    'matplotlib',
+    'pandas',
+    'numpy',
+    'catboost' 
+    ]
+
+VERSION = "0.0.0.1"
+
+setup(
+    name='pysmatch',
+    packages=['pysmatch'],
+    version=VERSION,
+    description='Matching techniques for Observational Studies',
+    author='Miao HanCheng',
+    author_email='hanchengmiao@gmail.com',
+    url='https://github.com/mhcone/pysmatch',
+    download_url='https://github.com/mhcone/pysmatch/archive/{}.tar.gz'.format(VERSION),
+    keywords=['logistic', 'regression', 'matching', 'observational', 'study', 'causal', 'inference','pysmatch'],
+    include_package_data=True,
+    install_requires=dependencies
+)
