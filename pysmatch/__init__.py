bb21cd06cbad68928c00f201ae6f4147c6cdb412
miaohancheng <miaohancheng@yeah.net>
2025-04-03 00:19:48 +0800

    Update __init__.py

--- pysmatch/__init__.py
+++ pysmatch/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
+
+from .Matcher import Matcher
+from . import utils
+from . import modeling
+from . import matching
+from . import visualization
+__version__ = "1.6"
